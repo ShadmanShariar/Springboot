f3df58c026d100ea8827cc403e19853da1c8f8c7
Shadman Shariar <75669228+ShadmanShariar@users.noreply.github.com>
2026-06-30 23:45:22 +0600

    first commit

---
+++ src/test/java/com/enterprise/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterprise.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
